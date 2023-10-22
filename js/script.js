27452ea4cc51d48dee56ad6b7f69656d3cc1e8f0
abdelghaniAit <aaittamghart8@gmail.com>
2023-10-22 18:15:37 +0100

    Add search modal

--- js/script.js
+++ js/script.js
@@ -1,15 +1,51 @@
-/* Menu button */
-const navBtn = document.querySelector(".nav-btn-svg");
-const navBox = document.querySelector(".navbar-box");
+let header = document.querySelector(".header-section");
+let navBtn = document.querySelector(".header-btn.nav-btn");
+let navBar = document.querySelector(".navbar-box");
+let overlay = document.querySelector(".overlay");
+
+/* Mobile menu */
 
 // button event
 navBtn.addEventListener('click', () => {
-    navBox.classList.toggle("open");
+    if (navBar.classList.toggle("open")){
+        overlay.style.top = header.offsetHeight + "px";
+        overlay.style.display = "block";
+    }else
+        overlay.style.display = "none";
+})
+
+
+/* Search */
+let searchBtn = document.querySelector("#search-btn");
+let searchModal = document.querySelector(".search-modal");
+let closeBtn = document.querySelector(".btn-close");
+
+// search button
+searchBtn.addEventListener("click", () => {
+    searchModal.style.display = "flex";
+    overlay.style.display = "block";
+})
+
+// close button
+closeBtn.addEventListener('click', () => {
+    overlay.style.display = "none";
+    searchModal.style.display = "none";
 })
 
 // Document event
 document.addEventListener("click", (event) => {
-    if (!navBox.contains(event.target) && event.target !== navBtn) {
-      navBox.classList.remove("open");
+    if (navBar.classList.contains("open") && !navBar.contains(event.target) && !navBtn.contains(event.target)) {
+        overlay.style.display = "none";
+        overlay.style.top = "0";
+        navBar.classList.remove("open");
+    }
+
+    if (searchBtn.contains(event.target)){
+        overlay.style.display = "block";
+    }
+
+    if (event.target == overlay){
+        searchModal.style.display = "none";
+        overlay.style.display = "none";
     }
 });
